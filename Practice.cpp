7f0fd1b724f42b0a8fc07529c4c0023d0c638782
root <root@vl850-s-1-162-dhcp.csuchico.edu>
2020-04-25 22:35:48 -0700

    empty string returns false for palindrome

--- Practice.cpp
+++ Practice.cpp
@@ -32,6 +32,9 @@ void Practice::sortDescending(int & first, int & second, int & third)
 // character in the string, but disregarding case ('x' is the same as 'X')
 bool Practice::isPalindrome(string input)
 {
+  if(input.size() == 0){ // empty string is not palindrome
+	return false;
+  }
   for(int i=0; i < input.size(); i++)
   {
     if( input[i] < 'A' || input[i] > 'Z' )
